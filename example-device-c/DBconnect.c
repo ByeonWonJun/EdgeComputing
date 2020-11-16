88286dc327c05aec5c10896f99446dde0bef7184
ByeonWonJun <won_jan@hanmail.net>
2020-11-16 02:25:19 -0800

    AddSample1.6

--- example-device-c/DBconnect.c
+++ example-device-c/DBconnect.c
@@ -11,38 +11,45 @@ void main(){
 	char *server = "localhost";
 	char *usr = "root";
 	char *password = "78590q";
-	char *database = "new_tracking"
+	char *database = "new_tracking";
 
-	if(conn=mysql_init((MYSQL*)!=NULL)){
-		fprintf("init fail\n");
-		return 1;
+	if(!(conn=mysql_init((MYSQL*)NULL))){
+		printf("init fail\n");
+		exit(1);
 	}
 
-	fprintf("mysql_init sucsess.\n");
+	printf("mysql_init sucsess.\n");
 	
 	if(mysql_real_connect(conn,server,usr,password,NULL,3306,NULL,0)){
-		fprintf("connect error.\n");
-		return 1;
+		printf("connect error.\n");
+		exit(1);
 	}
 
-	fprintf("mysql_real_connect sucsess.\n");
+	printf("mysql_real_connect sucsess.\n");
+	
+	if(mysql_query(conn,"INSERT INTO new_tracking.new_table VALUES(1,2)")){
+		finish_with_error(conn);	
 
+	}
+/*
 	if(mysql_select_db(conn,database)!=0){
 		mysql_close(conn);
-		fprintf("select_db fail.\n");
-		return 1;
+		printf("select_db fail.\n");
+		exit(1);
 	}
-	fprintf("select db sucsess.\n");
-
+	printf("select db sucsess.\n");
+*/
+/*
 	if(mysql_query(conn,"select* from new_tracking.new_table")){
-		fprintf("query fail\n");
-		return 1;
+		printf("query fail\n");
+		exit(1);
 	}
 
-	fprintf("query fail\n");
-
+	printf("query fail\n");
+*/
+/*
 	res = mysql_store_result(conn);
-	fprintf("res sucsess\n");
-
+	printf("res sucsess\n");
+*/
 	mysql_close(conn);
 }
