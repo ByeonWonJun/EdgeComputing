919cb25c4a1e18b069a749b7a9819ebc34f10c24
ByounWonJun <won_jan@hanmail.net>
2020-11-18 16:36:52 -0800

    EdgeX3.2

--- example-device-c/template.c
+++ example-device-c/template.c
@@ -118,9 +118,6 @@ static bool template_get_handler
 		exit(1);
 	}
 	
-	
-
-  
 
   /* Access the location of the device to be accessed and log it */
   iot_log_debug(driver->lc, "GET on device:");
@@ -209,7 +206,7 @@ static bool template_put_handler
         iot_log_debug (driver->lc, "  Value: %s", values[i].value.string_result);
         break;
       case Uint8:
-        iot_log_debug (driver->lc, "  Value: %d", values[i].value.ui8_result);
+        iot_log_debug (driver->lc, "  Valuellll: %d", values[i].value.ui8_result);
         break;
       case Bool:
         iot_log_debug (driver->lc, "  Value: %s", values[i].value.bool_result ? "true" : "false");
