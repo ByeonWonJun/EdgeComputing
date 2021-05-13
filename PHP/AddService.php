c5a57b0e265bffdaaddaecf7a03fe8149897a6a7
ByounWonJun <won_jan@hanmail.net>
2021-05-13 15:12:18 +0900

    Add files via upload

--- PHP/AddService.php
+++ PHP/AddService.php
@@ -6,13 +6,26 @@
     $DeviceID = $_POST["DeviceID"];
     $ServiceID = $_POST["ServiceID"];
 
-    $statement = mysqli_prepare($con, " UPDATE tracking.mapping SET ServiceID= ? WHERE DeviceID= ? AND UserID = ?");
-    mysqli_stmt_bind_param($statement, "sss", $ServiceID, $UserID, $DeviceID);
-    mysqli_stmt_execute($statement);
-
+    $isNull = mysqli_query($con, " SELECT ServiceID FROM tracking.mapping WHERE DeviceID = '$DeviceID' AND UserID = '$UserID'");
     $response = array();
-    $response["success"] = true;
 
+    if($isNull == null){
+        $statement = mysqli_prepare($con, " UPDATE tracking.mapping SET ServiceID= ? WHERE DeviceID= ? AND UserID = ?");
+        mysqli_stmt_bind_param($statement, "sss", $ServiceID, $DeviceID, $UserID);
+        mysqli_stmt_execute($statement);
+        $response["success"] = true;
+    }else if($isNull != null){
+        $statement = mysqli_prepare($con, " INSERT INTO tracking.mapping VALUES(?,?,?)");
+        mysqli_stmt_bind_param($statement, "sss", $UserID,$DeviceID, $ServiceID );
+        mysqli_stmt_execute($statement); 
+        $response["success"] = true;
+    }else{
+        $response["success"] = false;
+    }
+    
     echo json_encode($response);
+
     mysqli_close($con);
+
+
 ?>
