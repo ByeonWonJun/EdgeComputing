8aff746a7e3fea9d7b30816e9e6a7e24742986a6
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-05-13 13:44:10 +0900

    Update AppDelete.php

--- PHP/AppDelete.php
+++ PHP/AppDelete.php
@@ -1,33 +1,33 @@
 <?php
-    $con = mysqli_connect("localhost", "tracking", "system", "tracking");
-    mysqli_query($con,'SET NAMES utf8');
+$con = mysqli_connect("localhost", "tracking", "system", "tracking");
+mysqli_query($con,'SET NAMES utf8');
 
-    $DeviceID = $_POST["DeviceID"];
-    $ServiceID = $_POST["ServiceID"];
+$DeviceID = $_POST["DeviceID"];
+$ServiceID = $_POST["ServiceID"];
 
-	$count_statement = mysqli_query($con, "Select Distinct FileID FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
-	$count = mysqli_num_rows($count_statement);
+$count_statement = mysqli_query($con, "Select Distinct FileID FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
+$count = mysqli_num_rows($count_statement);
 
-	$response = array();
+$response = array();
 
-	if($count >= 1){
-		$NFS_statement = mysqli_query($con, "Select Distinct FileID FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
-		while($row=mysqli_fetch_assoc($NFS_statement)){
-			$command = $row["FileID"];
-			system("sudo /home/tracking/Deletion/Deletion '$command'");
-		}
-
-		$statement = mysqli_query($con, "DELETE FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
-		$statement = mysqli_query($con, "DELETE FROM mapping WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
-
-		$response["success"] = true;
+if($count >= 1){
+	$NFS_statement = mysqli_query($con, "Select Distinct FileID FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
+	
+	while($row=mysqli_fetch_assoc($NFS_statement)){
+		$command = $row["FileID"];
+		system("sudo /home/tracking/Deletion/Deletion '$command'");
 	}
 
-	else{
-		$response["success"] = false;
-	}
+	$statement = mysqli_query($con, "DELETE FROM tracking WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
+	$statement = mysqli_query($con, "DELETE FROM mapping WHERE DeviceID = '$DeviceID' AND ServiceID = '$ServiceID'");
+	
+	$response["success"] = true;
+}
 
-    echo json_encode($response);
+else{
+	$response["success"] = false;
+}
 
-    mysqli_close($con);
+echo json_encode($response);
+mysqli_close($con);
 ?>
